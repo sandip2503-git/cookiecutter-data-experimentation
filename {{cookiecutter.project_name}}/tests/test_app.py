fb5a6e0ec2b2d112d12c64c1b5b3443f913ca615
sandip2503 <sandipsaha.dba@gmail.com>
2025-07-18 11:23:26 +0530

    Update test_app.py

--- {{cookiecutter.project_name}}/tests/test_app.py
+++ {{cookiecutter.project_name}}/tests/test_app.py
@@ -1,9 +1,1 @@
-import duckdb
-from {{cookiecutter.project_slug}}.app import get_data
-from {{cookiecutter.project_slug}}.settings import DATA_LOCATION
 
-
-def test_get_data():
-    duckdb_conn = duckdb.connect()
-    df = get_data(duckdb_conn, file=f'{DATA_LOCATION}/example_data.csv')
-    assert df.shape[0] == 2
